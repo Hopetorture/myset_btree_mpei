35dc78c5b6e768b5167521f227d198852b56d114
Sergey <gss@dellhost.delldomain>
2017-09-01 18:33:09 +0300

    Initial commit

---
+++ src/mpei/BTreeSet.java
@@ -0,0 +1,4 @@
+package mpei;
+
+public class BTreeSet {
+}
---
+++ src/mpei/Main.java
@@ -0,0 +1,9 @@
+package mpei;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+    }
+}
