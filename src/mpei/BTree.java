375f87eb6751c055b92b400cef22faf2cf1b0630
Gorodetsky Sergey <sergeifakow@rambler.ru>
2017-09-04 00:30:28 +0300

    added add,find and partly delete functions

--- src/mpei/BTree.java
+++ src/mpei/BTree.java
@@ -1,4 +1,97 @@
 package mpei;
 
-public class BTree {
+import javax.lang.model.type.NullType;
+
+public class BTree<DataType extends Comparable> {
+    public  BTree(){
+        this.root = null;
+    }
+
+    public void add(DataType d){
+        if(root == null)
+            root = new Node<DataType>(null, d);
+        else
+            root.add(d);
+    }
+    public boolean find(DataType d){
+        if (root == null) return false;
+        return root.find(d);
+    }
+
+
+
+    private Node<DataType> root;
+    private class Node<T extends Comparable> {
+        public Node(Node parent, T data){
+            this.left = null;
+            this.right = null;
+            this.parent = parent;
+            this.data = data;
+        }
+
+        public void add(T newData){
+            int compareResult = data.compareTo(newData);
+            switch (compareResult){
+                case  -1:           // data < newData //add to right
+                    if (right != null)
+                        right.add(newData);
+                    else
+                        right = new Node(this, newData);
+                    break;
+                case   0:  return;  //already exists!
+                case   1:           // data > newData //add to left
+                    if (left != null)
+                        left.add(newData);
+                    else
+                        left = new Node(this, newData);
+                    break;
+                default : throw new RuntimeException();
+            }
+        }
+
+        public boolean find(T fd){
+            int compareResult = this.data.compareTo(fd);
+            switch (compareResult){
+                case -1:              // data < fd - find in right sub-tree
+                    if (right != null)
+                        return right.find(fd);
+                    break;
+                case 0: return true; //found
+                case 1:              //data  > fd - find in left sub-tree
+                    if (left != null)
+                        return left.find(fd);
+                    break;
+
+                default: return false;
+            }
+            return false;
+        }
+
+        public boolean rm(T fd){
+            int compareResult = this.data.compareTo(fd);
+            switch (compareResult){
+                case -1:
+                    if (right != null)
+                        return right.rm(fd);
+                    else break;
+                case 0: return
+                case 1:
+                    if (left != null)
+                        return left.rm(fd);
+                    else break;
+                default: break;
+            }
+            return false;
+        }
+
+        private boolean rm_help(){
+            if (this.left == null && this.right == null) ;
+        }
+
+        public Node parent;
+        public Node left;
+        public Node right;
+        public T data;
+    }
+
 }
--- src/mpei/Main.java
+++ src/mpei/Main.java
@@ -1,9 +1,53 @@
 package mpei;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello world");
+        //Integer x = 5;
+        //System.out.println(x.compareTo(10)); // compareTo=spaceship operator
+
+        BTree<Integer> itest = new BTree<>();
+        itest.add(50);
+        itest.add(45);
+        itest.add(5);
+        itest.add(1);
+        itest.add(100);
+
+        System.out.print(" 100 ");
+        System.out.println(itest.find(100));
+
+        System.out.print(" 101 ");
+        System.out.println(itest.find(101));
+
+        System.out.print(" 1 ");
+        System.out.println(itest.find(1));
+
+        System.out.print(" 45 ");
+        System.out.println(itest.find(45));
+
+        System.out.print(" 46 ");
+        System.out.println(itest.find(46));
+
+        /*Integer a,b;
+        a = 50;
+        b = 10;
+        int res = a.compareTo(b);
+        System.out.println(res); // a < b  => -1;  a > b => 1; a=b => 0
+        */
+        /*
+        BTreeSet<Integer> itest = new BTreeSet<Integer>();
+        itest.test(10,20);
+        BTreeSet<String> stest = new BTreeSet<String>();
+        stest.test("abc", "abc");
+        List<Integer> lint = Arrays.asList(5, 6, 7, 8, 9, 10);
+        */
+
+
+
     }
 }
